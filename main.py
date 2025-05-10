39decdbd301d3010bbc41fbc637b56f23529e22a
grison <67502047+THEgrison@users.noreply.github.com>
2025-05-10 17:02:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-import os
 import discord
+import os
+import asyncio
 from discord.ext import commands
+from discord import app_commands
 from flask import Flask
+from threading import Thread
 
 # --- Serveur Flask pour UptimeRobot ---
 app = Flask('')
 
-# Route principale qui renvoie un message pour vérifier que le bot est vivant
+
 @app.route('/')
 def home():
-    return "Je suis vivant !"  # Message envoyé pour la vérification d'UptimeRobot
+    return "Je suis vivant !"
+
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+
+Thread(target=run).start()
 
 # --- Discord Bot ---
 
-# Charger le token depuis les variables d'environnement
+# Charger le token depuis les variables d'environnement (Replit Secrets)
 TOKEN = os.environ['TOKEN']
 
 # Définir les intents nécessaires
@@ -23,6 +33,13 @@ intents.message_content = True
 # Initialiser le bot
 bot = commands.Bot(command_prefix="/", intents=intents, help_command=None)
 
+
+# Vérification globale : Seuls les administrateurs peuvent exécuter les commandes
+@bot.check
+async def globally_admin_only(ctx):
+    return ctx.author.guild_permissions.administrator
+
+
 # Quand le bot est prêt
 @bot.event
 async def on_ready():
@@ -30,6 +47,7 @@ async def on_ready():
     await bot.tree.sync()
     print("📜 Commandes chargées :",
           [command.name for command in bot.tree.get_commands()])
+
 
 # Charger automatiquement les fichiers de cogs
 async def load_extensions():
@@ -42,17 +60,13 @@ async def load_extensions():
             except Exception as e:
                 print(f"❌ Erreur lors du chargement de {filename}: {e}")
 
-# Fonction principale pour démarrer le bot
+
+# Fonction principale
 async def main():
     async with bot:
         await load_extensions()
         await bot.start(TOKEN)
 
-# Lancer le bot
-import asyncio
-asyncio.run(main())
 
-# Lancer le serveur Flask (sans thread, puisque Gunicorn s'en charge)
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))  # Railway définit cette variable automatiquement
-    app.run(host='0.0.0.0', port=port)
+# Lancer le bot
+asyncio.run(main())
