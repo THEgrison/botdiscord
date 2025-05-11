642f99ca8ee411bb546fd6e2dd552ec61009db29
grison <67502047+THEgrison@users.noreply.github.com>
2025-05-11 16:29:42 +0200

    Update warnings.py

--- commands/warnings.py
+++ commands/warnings.py
@@ -6,12 +6,12 @@ from discord import app_commands
 class Warnings(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.warns = bot.get_cog("Warn").warnings  # Récupère les avertissements du cog Warn
+        self.warns = bot.get_cog("Warn").warnings
 
     @app_commands.command(name="warnings", description="Affiche les avertissements d’un membre")
     @app_commands.checks.has_permissions(manage_messages=True)
     async def warnings(self, interaction: discord.Interaction, member: discord.Member):
-        user_warns = self.warns.get(member.id)
+        user_warns = self.warns.get(str(member.id))
 
         if not user_warns:
             await interaction.response.send_message(f"✅ {member.mention} n'a aucun avertissement.", ephemeral=True)
