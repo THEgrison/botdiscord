0aab90498668fda358611e83cff0d590ea5e79da
grison <67502047+THEgrison@users.noreply.github.com>
2025-05-11 16:30:03 +0200

    Update clearwarns.py

--- commands/clearwarns.py
+++ commands/clearwarns.py
@@ -6,16 +6,18 @@ from discord import app_commands
 class ClearWarns(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.warns = bot.get_cog("Warn").warnings  # Récupère les avertissements du cog Warn
+        self.warns = bot.get_cog("Warn").warnings
+        self.save = bot.get_cog("Warn").save_warnings
 
     @app_commands.command(name="clearwarns", description="Supprime tous les avertissements d’un membre")
     @app_commands.checks.has_permissions(manage_messages=True)
     async def clearwarns(self, interaction: discord.Interaction, member: discord.Member):
-        if member.id not in self.warns or len(self.warns[member.id]) == 0:
+        if str(member.id) not in self.warns or len(self.warns[str(member.id)]) == 0:
             await interaction.response.send_message(f"{member.mention} n’a aucun avertissement à supprimer.", ephemeral=True)
             return
 
-        del self.warns[member.id]
+        del self.warns[str(member.id)]
+        self.save()
 
         await interaction.response.send_message(f"✅ Tous les avertissements de {member.mention} ont été supprimés.")
 
